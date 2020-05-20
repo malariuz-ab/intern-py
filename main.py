792b8c8066c9352af240ff967f651d052076dd78
malariuz <sb-101@yandex.ru>
2020-05-20 15:58:41 +0700

    Firts update

--- main.py
+++ main.py
@@ -11,8 +11,25 @@ number = 0
 userNumber = int(input('Type some number: '))
 while number <= userNumber:
     if number % 2 == 0 and number != 0:
-        print('The currenct number is', end=' ')
+        print('The currencе number is', end=' ')
         print(number)
     elif number == 0:
         print('Wow! It\'s zero!')
     number += 1
+
+# Multitudes
+# семейная пара собирается в отпуск
+# каждый из супругов в поездку вещи
+# Max взял эти вещи
+max_things = {'Телефон', 'Бритва', 'Рубашка', 'Шорты'}
+# Kate взяла эти вещи
+kate_things = {'Телефон', 'Шорты', 'Зонтик', 'Помада'}
+
+# какие вещи взяли супруги?
+print(max_things | kate_things)
+# найти повторяющиеся вещи
+print(max_things & kate_things)
+# какие вещи взял Max, но не взяла Kate
+print(max_things - kate_things)
+# какие вещи взяла Kate, но не взял Max
+print(kate_things - max_things)
